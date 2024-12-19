63fe414207037c91350394d8bce746809de61379
Ваше Имя <you@example.com>
2024-12-19 16:44:34 +0400

    6 commit

--- components/templates/ContactsPage/ContactsPage.tsx
+++ components/templates/ContactsPage/ContactsPage.tsx
@@ -14,58 +14,63 @@ const ContactsPage = ({ isWholesaleBuyersPage = false }) => {
     <section className={styles.contacts}>
       <div className="container">
         <h2 className={`${styles.contacts__title} ${darkModeClass}`}>
-          {isWholesaleBuyersPage ? 'Оптовым покупателям' : 'Контакты'}
+          {isWholesaleBuyersPage ? 'Сотрудничество' : 'Контакты'}
         </h2>
         <div className={styles.contacts__inner}>
           {isWholesaleBuyersPage ? (
             <div className={`${styles.contacts__list} ${darkModeClass}`}>
               <p>
                 <span>
-                  Условия оптовых заказов решаются индивидуально по телефону:{' '}
+                  Наше сотрудничество можем обсудить по телефону:{' '}
                 </span>
-                <span>+7 (555) 55-55-555</span>
+                <span>+7 (999) 999-99-99</span>
               </p>
               <p>
-                Либо опишите суть заказа в форме обртной связи и мы с вами
-                свяжемся.
+                <span>
+                  Или можете прислать предложение на нашу почту:{' '}
+                </span>
+                <span>kostya.murin@gmail.com</span>
+              </p>
+              <p>
+                Либо напишите нам в форму обратной связи.
               </p>
             </div>
           ) : (
             <ul className={`${styles.contacts__list} ${darkModeClass}`}>
               <li className={styles.contacts__list__title}>
                 <h3 className={darkModeClass}>
-                  Магазин деталей для газовых котлов
+                  ХРОМ Детеилс - все для вашего дома!
                 </h3>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
+                <span>Наш магазин:</span>
+                <span> г. Ижевск, ул. Ижевская д 44</span>
+              </li>
+              <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
                 <span>Офис:</span>
-                <span> г. Москва, ул. ... д....</span>
+                <span> г. Ижевск, ул. Ижевская д 44</span>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
-                <span>Склад:</span>
-                <span> г. Москва, ул. ... д....</span>
-              </li>
-              <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
-                <span>График работы офиса:</span>
-                <span> пн-пс: с 8:00 до 22:00</span>
+                <span>Работаем ежедневно для вас:</span>
+                <span> с 8:00 до 20:00</span>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
                 <span>Наш контактный телефон:</span>
-                <span> +7(8095) 555-55-55</span>
+                <span> +7(999) 999-99-99</span>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
-                <span>Время приемок завок:</span>
-                <span> Пн-Вс: с 8:00 до 22:00</span>
+                <span>Принимаем ваши заявки:</span>
+                <span> с 8:00 до 20:00</span>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
-                <span>Прием заказов электронным способом на сайте:</span>
-                <span> круглосуточно</span>
+                <span>Прием заказов на сайте:</span>
+                <span> 24 часа</span>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
                 <span>E-mail:</span>
                 <span className={styles.contacts__list__item__mail}>
                   {!isMobile560 && <MailSvg />}{' '}
-                  <span>info@zapchasti.com.ru</span>
+                  <span>kostya.murin@gmail.com</span>
                 </span>
               </li>
             </ul>
