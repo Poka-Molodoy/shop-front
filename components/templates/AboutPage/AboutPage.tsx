a2583bc08f08cdeb92ff76af091c24d32b8faf6e
Ваше Имя <you@example.com>
2024-12-19 19:04:54 +0400

    footer fix 5 commit

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -11,29 +11,24 @@ const AboutPage = () => {
     <section className={styles.about}>
       <div className="container">
         <h2 className={`${styles.about__title} ${darkModeClass}`}>
-          О компании
+          О нас
         </h2>
         <div className={styles.about__inner}>
-          <div className={`${styles.about__info} ${darkModeClass}`}>
-            <p>
-              Компания &quot;АкваТермикс&quot; предлагает Вам запасные части для
-              европейских, корейских и отечественных газовых и электрических
-              котлов. 99% запчастей представленных на сайте постоянно
-              поддерживаются в наличии на нашем складе.
-            </p>
-            <p>
-              Ассортимент интернет-магазина &quot;АкваТермикс&quot; включает в
-              себя запасные части для котлов Arderia, Ariston, Baxi, Beretta,
-              Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,
-              Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida,
-              Saunier Duval, Sime, Tiberis, Vaillant, Viessmann, Westen.
-            </p>
-          </div>
           <div className={`${styles.about__img} ${styles.about__img__top}`}>
-            <img src="/img/about-img.png" alt="image-1" />
+            <img src="/img/lycs-architecture-U2BI3GMnSSE-unsplash.jpg" alt="image-1" />
+          </div>
+          <div className={`${styles.about__info} ${darkModeClass}`}>
+            <p> Интернет-магазин &quot;ХРОМ Детеилс&quot;
+              предлагает широкий ассортимент запасных частей для газовых и электрических котлов европейского,
+              корейского и отечественного производства. Мы гарантируем, что 99% запчастей, представленных на нашем сайте,
+              всегда доступны на складе. </p>
+            <p> В нашем каталоге вы найдете детали таких брендов,
+              как Arderia, Ariston, Baxi, Beretta, Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad,
+              Electrolux, Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida, Saunier Duval,
+              Sime, Tiberis, Vaillant, Viessmann и Westen. </p>
           </div>
           <div className={`${styles.about__img} ${styles.about__img__bottom}`}>
-            <img src="/img/about-img-2.png" alt="image-2" />
+            <img src="/img/austin-distel-wawEfYdpkag-unsplash.jpg" alt="image-2" />
           </div>
         </div>
       </div>
